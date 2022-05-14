284c8a4b3e0e14f38f6bf54a3a92fbe79780b86f
Marc Eisenbarth <marc.eisenbarth.sb@googlemail.com>
2022-05-14 09:31:58 +0200

    some keymap changes

--- keyboards/dztech/dz60rgb/keymaps/meisenbar/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/meisenbar/keymap.c
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         TO(3),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, _______,   _______, _______, _______, KC_CALC, RALT(KC_Y), KC_INS,  RALT(KC_P), KC_PSCR, KC_SLCK, KC_PAUS, RESET,
-        _______, RALT(KC_Q), RALT(KC_S), _______, _______, _______, _______, _______, _______, _______, KC_HOME, RALT(KC_GRV),          EEP_RST,
+        _______, RALT(KC_Q), RALT(KC_S), _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_GRV,          EEP_RST,
         _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, KC_END,  KC_RSFT, KC_VOLU, KC_MUTE,
         _______, _______, _______,                            TO(4),                     KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_MNXT
     ),
     [2] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, RESET,
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          EEP_RST,
+        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______,  KC_GRV,          EEP_RST,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______
     ),
